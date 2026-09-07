5e407446e92cfcc98ced481fb37d60063e69c2b2
Noname267 <sladepal22@gmail.com>
2026-09-07 13:31:28 +0300

    Update EnvVarsView.java

--- app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
+++ app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
@@ -44,6 +44,7 @@ public class EnvVarsView extends FrameLayout {
         {"GALLIUM_HUD", "SELECT_MULTIPLE", "simple", "fps", "frametime"},
         {"WINE_LARGE_ADDRESS_AWARE", "CHECKBOX", "0", "1"},
         {"WINEDLLOVERRIDES", "TEXT"},
+        {"DXVK_FRAME_RATE", "NUMBER"},
         {"DXVK_CONFIG_FILE", "TEXT"},
         {"DXVK_STATE_CACHE", "CHECKBOX", "0", "1"},
         {"DXVK_LOG_LEVEL", "SELECT", "none", "error", "warn", "info", "debug"},
