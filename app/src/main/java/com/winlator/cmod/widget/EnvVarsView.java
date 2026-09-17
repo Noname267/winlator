9dc4fa8716680c8cdf016084a41399588ae09ea2
Noname267 <sladepal22@gmail.com>
2026-09-17 19:29:02 +0300

    Update EnvVarsView.java

--- app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
+++ app/src/main/java/com/winlator/cmod/widget/EnvVarsView.java
@@ -48,8 +48,6 @@ public class EnvVarsView extends FrameLayout {
         {"DXVK_CONFIG_FILE", "TEXT"},
         {"DXVK_STATE_CACHE", "CHECKBOX", "0", "1"},
         {"DXVK_LOG_LEVEL", "SELECT", "none", "error", "warn", "info", "debug"},
-        {"PROTON_LOG", "CHECKBOX", "0", "1"},
-        {"MESA_DEBUG", "SELECT", "silent", "flush", "incomplete_tex", "incomplete_fbo", "context"},
         {"WRAPPER_NO_OPCONSTCOMP", "CHECKBOX", "0", "1"}
     };
     private final LinearLayout container;
