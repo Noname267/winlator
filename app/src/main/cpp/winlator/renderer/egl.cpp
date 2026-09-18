40d528b1cdcf99e9e739b51dfbcd0b453b2065e7
Pipetto-crypto <Pipetto-crypto@pipetto-crypto.com>
2026-09-18 14:26:41 +0200

    renderer_jni: improve synchronization between cursor events and egl rendering thread

--- app/src/main/cpp/winlator/renderer/egl.cpp
+++ app/src/main/cpp/winlator/renderer/egl.cpp
@@ -286,6 +286,7 @@ void EGLRenderer::renderCursor() {
     jint id = env->GetIntField(pointWindowObj, cache->windowID);
     auto pointWindow = windowManager->getWindow(id);
     auto cursor = (pointWindow != nullptr) ? pointWindow->cursor : nullptr;
+    
     int x = std::clamp(cursorManager->pointer.posX, 0, windowManager->getRootWindow()->width - 1);
     int y = std::clamp(cursorManager->pointer.posY, 0, windowManager->getRootWindow()->height - 1);
 
