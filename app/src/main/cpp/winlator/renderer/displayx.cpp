2180ec05eeaa24749cf30e429909ee3ea874a02c
Pipetto-crypto <Pipetto-crypto@pipetto-crypto.com>
2026-09-20 14:25:10 +0200

    displayx: avoid closing a null perfhint session

--- app/src/main/cpp/winlator/renderer/displayx.cpp
+++ app/src/main/cpp/winlator/renderer/displayx.cpp
@@ -552,7 +552,7 @@ void DisplayX::presentThreadLoop() {
         pfnASurfaceTransactionApply(presentTransaction);
     }
     
-    if (isPerformanceHintAPIAvailable()) {
+    if (isPerformanceHintAPIAvailable() && performanceHintSession) {
         pfnAPerformanceHintCloseSession(performanceHintSession);
     }
 }
